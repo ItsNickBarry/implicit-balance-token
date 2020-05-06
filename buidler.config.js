37234a854c9e976e716c9e88660a247dab74bd57
Nick Barry <itsnickbarry@protonmail.ch>
2020-05-06 18:41:35 -0500

    Initial commit

---
+++ buidler.config.js
@@ -0,0 +1,28 @@
+usePlugin('@nomiclabs/buidler-truffle5');
+usePlugin('buidler-gas-reporter');
+usePlugin('solidity-coverage');
+
+module.exports = {
+  solc: {
+    // version: '0.6.3',
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+
+  networks: {
+    generic: {
+      // set URL for external network, such as Infura
+      url: `${ process.env.URL }`,
+      accounts: {
+        mnemonic: `${ process.env.MNEMONIC }`,
+      },
+    },
+  },
+
+  gasReporter: {
+    enabled: !!process.env.REPORT_GAS,
+    gasPrice: 1,
+  },
+};
